d8c3d31fbcbf9a6f2e67985933649cd0675bf00d
telegram54654 <moviee3shub@gmail.com>
2024-10-18 18:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "28837789"))
+API_HASH = getenv("API_HASH", "33c9162294cdd6c9d51d964e4469fadb")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8087702564").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://smallcapitaltrader1:XVsdQj8vu38ZIFoy@cluster0.9g3ri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002445805057")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "2445805057"))
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "")
